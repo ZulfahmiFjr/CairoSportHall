3593c1a440249fe5d0097a70c2068839eacb06d8
ZulfahmiFjr <mccraft874@gmail.com>
2026-09-06 20:48:50 +0700

    nambahin fitur waktu

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
+// panggil lib jam internet bawaan esp32
+#include <time.h>
 // mobizt
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
@@ -20,6 +22,8 @@ const int relayPins[8] = {2, 12, 14, 27, 26, 25, 33, 32};
 unsigned long waktuDetakTerakhir = 0;
 // variabel buat nyimpen waktu cek firebase terakhir
 unsigned long waktuCekFirebaseTerakhir = 0;
+// variabel buat nyimpen waktu cek jadwal terakhir
+unsigned long waktuCekJadwalTerakhir = 0;
 void setup() {
   Serial.begin(115200);
   // setel semua pin jadi tukang ngeluarin setrum kyak output gitu
@@ -38,6 +42,8 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("konek mantapp!");
+  // sinkronin jam internet pakai wib utc plus tujuh biar mantap
+  configTime(7 * 3600, 0, "pool.ntp.org", "time.nist.gov");
   // nyiapin firebasenyaa
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
@@ -75,6 +81,40 @@ void loop() {
         }
       }
       waktuCekFirebaseTerakhir = waktuSekarang;
+    }
+    // timer jadwal jalan tiap enam puluh ribu milidetik alias semenit sekali
+    if (waktuSekarang - waktuCekJadwalTerakhir >= 60000) {
+      struct tm timeinfo;
+      // pastiin ngambil jam lokalnyaa sukses dulu
+      if (getLocalTime(&timeinfo)) {
+        // narik data pengaturan jadwal sekaligus biar kenceng
+        if (Firebase.RTDB.getJSON(&fbdo, "/jadwal")) {
+          FirebaseJson &jsonJadwal = fbdo.jsonObject();
+          FirebaseJsonData dataAktif, dataJamNyala, dataMenitNyala, dataJamMati, dataMenitMati, dataHari;
+          for (int i = 1; i <= 8; i++) {
+            String pathBase = "relay" + String(i);
+            jsonJadwal.get(dataAktif, pathBase + "/aktif");
+            jsonJadwal.get(dataJamNyala, pathBase + "/jamNyala");
+            jsonJadwal.get(dataMenitNyala, pathBase + "/menitNyala");
+            jsonJadwal.get(dataJamMati, pathBase + "/jamMati");
+            jsonJadwal.get(dataMenitMati, pathBase + "/menitMati");
+            String pathHari = pathBase + "/hari" + String(timeinfo.tm_wday);
+            jsonJadwal.get(dataHari, pathHari);
+            // pastiin semuanyaa bener bener dapet datanyaa dan jadwalku lagi nyala buat hari ini
+            if (dataAktif.success && dataAktif.boolValue && dataHari.success && dataHari.boolValue) {
+              // logika nyalain relay
+              if (dataJamNyala.success && dataMenitNyala.success && timeinfo.tm_hour == dataJamNyala.intValue && timeinfo.tm_min == dataMenitNyala.intValue) {
+                Firebase.RTDB.setInt(&fbdoHeartbeat, "/stopkontak/relay" + String(i), 1);
+              }
+              // logika matiin relay
+              if (dataJamMati.success && dataMenitMati.success && timeinfo.tm_hour == dataJamMati.intValue && timeinfo.tm_min == dataMenitMati.intValue) {
+                Firebase.RTDB.setInt(&fbdoHeartbeat, "/stopkontak/relay" + String(i), 0);
+              }
+            }
+          }
+        }
+      }
+      waktuCekJadwalTerakhir = waktuSekarang;
     }
   }
 }
