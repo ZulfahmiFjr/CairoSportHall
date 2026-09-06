7d816773230bbd0bddee887d751ab53e08af5a80
ZulfahmiFjr <mccraft874@gmail.com>
2026-09-06 09:46:07 +0700

    bagusin tampilan webnyaa

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,15 @@
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
 
-// Mobizt
+// mobizt
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
-// wifi yg dipake esp32
+// wifinyaa esp32 nih
 #define WIFI_SSID "anon"
 #define WIFI_PASSWORD "12345678"
 
-// copas dari file index.html
+// copasin aja dari file index.html
 #define API_KEY "AIzaSyCIPJKs36oEABoh_tRbMEpOELhGyx-Bq40"
 #define DATABASE_URL "cairosporthall-default-rtdb.asia-southeast1.firebasedatabase.app"
 
@@ -18,26 +18,26 @@ FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
-// daftar pin ESP32 buat disambung ke relay IN1 sampai IN8
+// daftarpin esp32 buat disambungin kerelay in1 sampe in8
 const int relayPins[8] = {2, 12, 14, 27, 26, 25, 33, 32};
 
 void setup() {
   Serial.begin(115200);
-  // setel semua pin jadi tukang ngeluarin setrum (output)
+  // setel semuapin jadi tukang ngeluarinsetrum kyak output gitu
   for(int i = 0; i < 8; i++) {
     pinMode(relayPins[i], OUTPUT);
-    // karnaa relaynyaa low trigger, dikasih high biar mati di awal
+    // karna relaynyaa tipenyaa lowtrigger trus biar mati diawal mending dikasih high
     digitalWrite(relayPins[i], HIGH);
   }
-  // konek ke WiFi
+  // nyambungin kewifi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Konek ke WiFi");
+  Serial.print("nyambungin kewifi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("Konek Mantap!");
-  // siapin Firebasenyaa
+  Serial.println("konekmantapbanget!");
+  // nyiapin firebasenyaa
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
   config.signer.test_mode = true;
@@ -47,19 +47,19 @@ void setup() {
 
 void loop() {
   if (Firebase.ready()) {
-    // ngecek mading firebase buat masing masing 8 tombolnyaa
+    // ngecekin datafirebase buat masingmasing delapan tombolnyaa
     for (int i = 1; i <= 8; i++) {
       String path = "/stopkontak/relay" + String(i);
       if (Firebase.RTDB.getInt(&fbdo, path)) {
         int status = fbdo.intData();
-        // logika kebalik buat low trigger
+        // logikakebalik kyak lowtrigger gitu
         if (status == 1) {
-          digitalWrite(relayPins[i-1], LOW); // cetek nyala
+          digitalWrite(relayPins[i-1], LOW); // bikinnyala
         } else {
-          digitalWrite(relayPins[i-1], HIGH); // cetek mati
+          digitalWrite(relayPins[i-1], HIGH); // bikinmati
         }
       }
     }
-    delay(500); // kasih jeda biar ESP32nyaa ngga kerja teruss
+    delay(500); // kasihjeda dikit biar esp32nyaa nggacapek kerjateruss
   }
 }
