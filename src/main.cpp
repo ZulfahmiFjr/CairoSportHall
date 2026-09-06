771aa5938eac1ecccfdef6097d62843074a868cc
ZulfahmiFjr <mccraft874@gmail.com>
2026-09-06 19:00:26 +0700

    nambahin fitur deteksi status online esp32 pakai logika heartbeat

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,22 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
-
 // mobizt
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
-
 // wifinyaa esp32 nih
 #define WIFI_SSID "anon"
 #define WIFI_PASSWORD "12345678"
-
 // copasin aja dari file index.html
 #define API_KEY "AIzaSyCIPJKs36oEABoh_tRbMEpOELhGyx-Bq40"
 #define DATABASE_URL "cairosporthall-default-rtdb.asia-southeast1.firebasedatabase.app"
-
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
-
 // daftarpin esp32 buat disambungin kerelay in1 sampe in8
 const int relayPins[8] = {2, 12, 14, 27, 26, 25, 33, 32};
-
+// variabel buat nyimpen waktu detak jantung terakhir
+unsigned long waktuDetakTerakhir = 0;
 void setup() {
   Serial.begin(115200);
   // setel semuapin jadi tukang ngeluarinsetrum kyak output gitu
@@ -44,22 +40,27 @@ void setup() {
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
 }
-
 void loop() {
   if (Firebase.ready()) {
-    // ngecekin datafirebase buat masingmasing delapan tombolnyaa
+    unsigned long waktuSekarang = millis();
+    // ngirim status kehidupan esp32 tiap lima detik sekali
+    if (waktuSekarang - waktuDetakTerakhir >= 5000) {
+      Firebase.RTDB.setInt(&fbdo, "/status/heartbeat", waktuSekarang);
+      waktuDetakTerakhir = waktuSekarang;
+    }
+    // ngecekin data firebase buat delapan tombolnyaa berurutan
     for (int i = 1; i <= 8; i++) {
       String path = "/stopkontak/relay" + String(i);
       if (Firebase.RTDB.getInt(&fbdo, path)) {
         int status = fbdo.intData();
-        // logikakebalik kyak lowtrigger gitu
+        // logika kebalik kyak low trigger gitu
         if (status == 1) {
-          digitalWrite(relayPins[i-1], LOW); // bikinnyala
+          digitalWrite(relayPins[i-1], LOW); // bikin nyala
         } else {
-          digitalWrite(relayPins[i-1], HIGH); // bikinmati
+          digitalWrite(relayPins[i-1], HIGH); // bikin mati
         }
       }
     }
-    delay(500); // kasihjeda dikit biar esp32nyaa nggacapek kerjateruss
+    delay(500); // kasih jeda dikit biar esp32nyaa ngga capek kerja terus
   }
 }
