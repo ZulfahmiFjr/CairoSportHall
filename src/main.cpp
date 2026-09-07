d8d803246ee673baccccf469b5e4e4c864099911
ZulfahmiFjr <mccraft874@gmail.com>
2026-09-07 22:48:35 +0700

    benerin bug popup alert error pas buka modal jadwal, alertnyaa digantiin ke console.log

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,39 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
+
 // panggil lib jam internet bawaan esp32
 #include <time.h>
+
 // mobizt
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
+
 // wifinyaa esp32 nih
-#define WIFI_SSID "zahira"
-#define WIFI_PASSWORD "qwerty123"
+#define WIFI_SSID "anon"
+#define WIFI_PASSWORD "12345678"
+
 // copasin aja dari file index.html
 #define API_KEY "AIzaSyCIPJKs36oEABoh_tRbMEpOELhGyx-Bq40"
 #define DATABASE_URL "cairosporthall-default-rtdb.asia-southeast1.firebasedatabase.app"
+
 FirebaseData fbdo;
 FirebaseData fbdoHeartbeat;
 FirebaseAuth auth;
 FirebaseConfig config;
+
 // daftarpin esp32 buat disambungin kerelay in1 sampe in8
 const int relayPins[8] = {2, 12, 14, 27, 26, 25, 33, 32};
+
 // variabel buat nyimpen waktu detak jantung terakhir
 unsigned long waktuDetakTerakhir = 0;
+
 // variabel buat nyimpen waktu cek firebase terakhir
 unsigned long waktuCekFirebaseTerakhir = 0;
-// variabel buat nyimpen waktu cek jadwal terakhir
-unsigned long waktuCekJadwalTerakhir = 0;
+
+// variabel baru buat nyimpen menit terakhir dicek biar ngga spam getjson jadwal
+int menitTerakhirDicek = -1;
+
 void setup() {
   Serial.begin(115200);
   // setel semua pin jadi tukang ngeluarin setrum kyak output gitu
@@ -52,13 +62,49 @@ void setup() {
   // ini buat reconnect wifi sama firebasenyaa otomatis kalau terputus
   Firebase.reconnectWiFi(true);
 }
+
 void loop() {
   if (Firebase.ready()) {
     unsigned long waktuSekarang = millis();
-    // ngirim status kehidupan esp32 tiap lima detik sekali pakai path stopkontak biar ngga diblokir rules
+    // ngecek timer lima detik buat ngirim heartbeat sekaligus ngecek jadwalnyaa
     if (waktuSekarang - waktuDetakTerakhir >= 5000) {
       Firebase.RTDB.setInt(&fbdoHeartbeat, "/stopkontak/heartbeat", waktuSekarang);
       waktuDetakTerakhir = waktuSekarang;
+      struct tm timeinfo;
+      // pastiin ngambil jam lokalnyaa sukses dulu
+      if (getLocalTime(&timeinfo)) {
+        // nah ini kuncinyaa biar getjson jadwalnyaa cuma dieksekusi pas menitnyaa udah ganti aja
+        if (timeinfo.tm_min != menitTerakhirDicek) {
+          // narik data pengaturan jadwal sekaligus biar kenceng
+          if (Firebase.RTDB.getJSON(&fbdo, "/jadwal")) {
+            FirebaseJson &jsonJadwal = fbdo.jsonObject();
+            FirebaseJsonData dataAktif, dataJamNyala, dataMenitNyala, dataJamMati, dataMenitMati, dataHari;
+            for (int i = 1; i <= 8; i++) {
+              String pathBase = "relay" + String(i);
+              jsonJadwal.get(dataAktif, pathBase + "/aktif");
+              jsonJadwal.get(dataJamNyala, pathBase + "/jamNyala");
+              jsonJadwal.get(dataMenitNyala, pathBase + "/menitNyala");
+              jsonJadwal.get(dataJamMati, pathBase + "/jamMati");
+              jsonJadwal.get(dataMenitMati, pathBase + "/menitMati");
+              String pathHari = pathBase + "/hari" + String(timeinfo.tm_wday);
+              jsonJadwal.get(dataHari, pathHari);
+              // pastiin semuanyaa bener bener dapet datanyaa dan jadwalku lagi nyala buat hari ini
+              if (dataAktif.success && dataAktif.boolValue && dataHari.success && dataHari.boolValue) {
+                // logika nyalain relay
+                if (dataJamNyala.success && dataMenitNyala.success && timeinfo.tm_hour == dataJamNyala.intValue && timeinfo.tm_min == dataMenitNyala.intValue) {
+                  Firebase.RTDB.setInt(&fbdoHeartbeat, "/stopkontak/relay" + String(i), 1);
+                }
+                // logika matiin relay
+                if (dataJamMati.success && dataMenitMati.success && timeinfo.tm_hour == dataJamMati.intValue && timeinfo.tm_min == dataMenitMati.intValue) {
+                  Firebase.RTDB.setInt(&fbdoHeartbeat, "/stopkontak/relay" + String(i), 0);
+                }
+              }
+            }
+            // update variabel menit terakhir dicek biar ngga ngulang getjson di lima detik berikutnyaa
+            menitTerakhirDicek = timeinfo.tm_min;
+          }
+        }
+      }
     }
     // ngecekin firebase tiap lima ratus milidetik kyak pengganti delay gitu
     if (waktuSekarang - waktuCekFirebaseTerakhir >= 500) {
@@ -81,40 +127,6 @@ void loop() {
         }
       }
       waktuCekFirebaseTerakhir = waktuSekarang;
-    }
-    // timer jadwal jalan tiap enam puluh ribu milidetik alias semenit sekali
-    if (waktuSekarang - waktuCekJadwalTerakhir >= 60000) {
-      struct tm timeinfo;
-      // pastiin ngambil jam lokalnyaa sukses dulu
-      if (getLocalTime(&timeinfo)) {
-        // narik data pengaturan jadwal sekaligus biar kenceng
-        if (Firebase.RTDB.getJSON(&fbdo, "/jadwal")) {
-          FirebaseJson &jsonJadwal = fbdo.jsonObject();
-          FirebaseJsonData dataAktif, dataJamNyala, dataMenitNyala, dataJamMati, dataMenitMati, dataHari;
-          for (int i = 1; i <= 8; i++) {
-            String pathBase = "relay" + String(i);
-            jsonJadwal.get(dataAktif, pathBase + "/aktif");
-            jsonJadwal.get(dataJamNyala, pathBase + "/jamNyala");
-            jsonJadwal.get(dataMenitNyala, pathBase + "/menitNyala");
-            jsonJadwal.get(dataJamMati, pathBase + "/jamMati");
-            jsonJadwal.get(dataMenitMati, pathBase + "/menitMati");
-            String pathHari = pathBase + "/hari" + String(timeinfo.tm_wday);
-            jsonJadwal.get(dataHari, pathHari);
-            // pastiin semuanyaa bener bener dapet datanyaa dan jadwalku lagi nyala buat hari ini
-            if (dataAktif.success && dataAktif.boolValue && dataHari.success && dataHari.boolValue) {
-              // logika nyalain relay
-              if (dataJamNyala.success && dataMenitNyala.success && timeinfo.tm_hour == dataJamNyala.intValue && timeinfo.tm_min == dataMenitNyala.intValue) {
-                Firebase.RTDB.setInt(&fbdoHeartbeat, "/stopkontak/relay" + String(i), 1);
-              }
-              // logika matiin relay
-              if (dataJamMati.success && dataMenitMati.success && timeinfo.tm_hour == dataJamMati.intValue && timeinfo.tm_min == dataMenitMati.intValue) {
-                Firebase.RTDB.setInt(&fbdoHeartbeat, "/stopkontak/relay" + String(i), 0);
-              }
-            }
-          }
-        }
-      }
-      waktuCekJadwalTerakhir = waktuSekarang;
     }
   }
 }
